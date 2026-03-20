c3c22b9e2a13504d74056827029c9ad65bf804d9
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-03-20 09:30:25 +0800

    Update helium.sh

--- helium/helium.sh
+++ helium/helium.sh
@@ -15,7 +15,7 @@ publicIP=$(ip -4 addr | sed -ne 's|^.* inet \([^/]*\)/.* scope global.*$|\1|p' |
 
 function header() {
 	echo -e $GREEN" $VERSIONNAME$VERSIONNUMBER" $NOCOLOR
-	echo -e $WHITE" by JUBAIR BRO" $NOCOLOR
+	echo -e $WHITE" by AMBERVPN" $NOCOLOR
 }
 
 function isRoot() {
