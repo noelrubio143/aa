600dfb8044d45dca9afb9af176ad5c2519ea15c2
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-03-20 09:54:07 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -23,7 +23,7 @@ commonname=none
 email=none
 
 # simple password minimal
-curl -sS https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
+curl -sS https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/password | openssl aes-256-cbc -d -a -pass pass:scvps07gg -pbkdf2 > /etc/pam.d/common-password
 chmod +x /etc/pam.d/common-password
 
 # go to root
@@ -161,9 +161,9 @@ apt -y install nginx
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
-wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT//master/ssh/nginx.conf"
+wget -O /etc/nginx/nginx.conf "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/nginx.conf"
 rm /etc/nginx/conf.d/vps.conf
-wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/vps.conf"
+wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/vps.conf"
 /etc/init.d/nginx restart
 
 mkdir /etc/systemd/system/nginx.service.d
@@ -174,13 +174,13 @@ service nginx restart
 cd
 mkdir /home/vps
 mkdir /home/vps/public_html
-wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/index"
-wget -O /home/vps/public_html/.htaccess "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/.htaccess"
+wget -O /home/vps/public_html/index.html "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/index"
+wget -O /home/vps/public_html/.htaccess "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/.htaccess"
 mkdir /home/vps/public_html/ss-ws
 mkdir /home/vps/public_html/clash-ws
 # install badvpn
 cd
-wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/newudpgw"
+wget -O /usr/bin/badvpn-udpgw "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/newudpgw"
 chmod +x /usr/bin/badvpn-udpgw
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7100 --max-clients 500' /etc/rc.local
 sed -i '$ i\screen -dmS badvpn badvpn-udpgw --listen-addr 127.0.0.1:7200 --max-clients 500' /etc/rc.local
@@ -297,7 +297,7 @@ echo 'Config file is at /usr/local/ddos/ddos.conf'
 echo 'Please send in your comments and/or suggestions to zaf@vsnl.com'
 
 # // banner /etc/issue.net
-wget -O /etc/issue.net "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/banner/banner.conf"
+wget -O /etc/issue.net "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/banner/banner.conf"
 echo "Banner /etc/issue.net" >> /etc/ssh/sshd_config
 sed -i 's@DROPBEAR_BANNER=""@DROPBEAR_BANNER="/etc/issue.net"@g' /etc/default/dropbear
 
@@ -321,43 +321,43 @@ netfilter-persistent reload
 # download script
 cd /usr/bin
 # menu
-wget -O menu "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/menu.sh"
-wget -O m-vmess "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-vmess.sh"
-wget -O m-vless "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-vless.sh"
-wget -O running "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/running.sh"
-wget -O clearcache "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/clearcache.sh"
-wget -O m-ssws "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-ssws.sh"
-wget -O m-trojan "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-trojan.sh"
+wget -O menu "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/menu.sh"
+wget -O m-vmess "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-vmess.sh"
+wget -O m-vless "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-vless.sh"
+wget -O running "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/running.sh"
+wget -O clearcache "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/clearcache.sh"
+wget -O m-ssws "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-ssws.sh"
+wget -O m-trojan "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-trojan.sh"
 
 # menu ssh ovpn
-wget -O m-sshovpn "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-sshovpn.sh"
-wget -O usernew "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/usernew.sh"
-wget -O trial "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/trial.sh"
-wget -O renew "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/renew.sh"
-wget -O hapus "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/hapus.sh"
-wget -O cek "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/cek.sh"
-wget -O member "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/member.sh"
-wget -O delete "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/delete.sh"
-wget -O autokill "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/autokill.sh"
-wget -O ceklim "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/ceklim.sh"
-wget -O tendang "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/tendang.sh"
-wget -O sshws "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/sshws.sh"
-wget -O user-lock "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/user-lock.sh"
-wget -O user-unlock "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/user-unlock.sh"
+wget -O m-sshovpn "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-sshovpn.sh"
+wget -O usernew "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/trial.sh"
+wget -O renew "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/renew.sh"
+wget -O hapus "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/hapus.sh"
+wget -O cek "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/cek.sh"
+wget -O member "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/member.sh"
+wget -O delete "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/delete.sh"
+wget -O autokill "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/autokill.sh"
+wget -O ceklim "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/ceklim.sh"
+wget -O tendang "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/tendang.sh"
+wget -O sshws "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/sshws.sh"
+wget -O user-lock "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/user-lock.sh"
+wget -O user-unlock "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/user-unlock.sh"
 
 # menu system
-wget -O m-system "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-system.sh"
-wget -O m-domain "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-domain.sh"
-wget -O add-host "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/add-host.sh"
-wget -O certv2ray "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/xray/certv2ray.sh"
-wget -O speedtest "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/speedtest_cli.py"
-wget -O auto-reboot "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/auto-reboot.sh"
-wget -O restart "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/restart.sh"
-wget -O bw "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/bw.sh"
-wget -O m-tcp "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/tcp.sh"
-wget -O xp "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/xp.sh"
-wget -O sshws "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/ssh/sshws.sh"
-wget -O m-dns "https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/menu/m-dns.sh"
+wget -O m-system "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-system.sh"
+wget -O m-domain "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-domain.sh"
+wget -O add-host "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/add-host.sh"
+wget -O certv2ray "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/xray/certv2ray.sh"
+wget -O speedtest "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/speedtest_cli.py"
+wget -O auto-reboot "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/auto-reboot.sh"
+wget -O restart "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/restart.sh"
+wget -O bw "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/bw.sh"
+wget -O m-tcp "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/tcp.sh"
+wget -O xp "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/xp.sh"
+wget -O sshws "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/ssh/sshws.sh"
+wget -O m-dns "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-dns.sh"
 
 chmod +x menu
 chmod +x m-vmess
