fa244d483d994f716c04a637f58c0953e37ea47f
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-03-20 09:32:32 +0800

    Update ins-helium.sh

--- helium/ins-helium.sh
+++ helium/ins-helium.sh
@@ -8,15 +8,15 @@ NC='\e[0m'
 clear
 rm -rf /usr/bin/m-system
 echo -e "\e[1;32m Update Menu System.. \e[0m"
-wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/helium/menu/m-system.sh
+wget -q -O /usr/bin/m-system https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/helium/menu/m-system.sh
 chmod +x /usr/bin/m-system
 rm -rf /usr/bin/wbmn
 echo -e "\e[1;32m Start download Panel Webmin.. \e[0m"
-wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/helium/webmin/wbmn.sh
+wget -q -O /usr/bin/wbmn https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/helium/webmin/wbmn.sh
 chmod +x /usr/bin/wbmn
 rm -rf /usr/bin/helium
 echo -e "\e[1;32m Start download Panel ADS Block.. \e[0m"
-wget -q -O /usr/bin/helium https://raw.githubusercontent.com/jubairbro/AUTO-SCRIPT/master/helium/helium.sh
+wget -q -O /usr/bin/helium https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/helium/helium.sh
 chmod +x /usr/bin/helium
 echo -e "\e[1;32m Setup done Please wait.. \e[0m"
 sleep 2
