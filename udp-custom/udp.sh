c377a55521259c03f3e6fea4c6401c6820ea5aec
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-04-02 11:25:13 +0800

    Update download link for udp-custom script

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -42,7 +42,7 @@ clear
 echo "\e[1;32m Please Waiting Proses Downloading Toll UdpCustom.. \e[0m"
 sleep 1
 clear
-wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/jubairbro/AUTO-SCRIPT/raw/master/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
+wget -q --show-progress --load-cookies /tmp/cookies.txt "https://github.com/noelrubio143/aa/refs/heads/main/udp-custom/udp-custom-linux-amd64" -O /root/udp/udp-custom && rm -rf /tmp/cookies.txt
 chmod +x /root/udp/udp-custom
 clear
 # install Config Default Udp
