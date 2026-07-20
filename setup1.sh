7e0882f0299a53b7c7dd90dd0bd41264c32692c4
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-20 23:45:49 +0800

    Remove unnecessary commands from setup1.sh

--- setup1.sh
+++ setup1.sh
@@ -114,7 +114,6 @@ echo "Contact: https://t.me/ambervpngc"
 echo "=================================================================="
 
 # Additional commands
-bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/slipstream-rust-deploy.sh
 bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/dnsdisable.sh)
 wget -O /root/log-install.txt https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/log-install.txt
 bash <(curl -Ls https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/dropbearconfig.sh)
