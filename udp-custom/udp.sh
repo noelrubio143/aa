6d73453fdcf64edef0f50f98546f50c52699e221
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-25 23:10:24 +0800

    Remove slowdns script from udp.sh
    Removed slowdns script download and execution.

--- udp-custom/udp.sh
+++ udp-custom/udp.sh
@@ -19,9 +19,6 @@ echo "\e[1;32m Update Menu.. \e[0m"
 sleep 1
 wget -q -O /usr/bin/usernew https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/udp-custom/ssh/usernew.sh
 wget -q -O /usr/bin/trial https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/udp-custom/ssh/trial.sh
-echo "\e[1;32m Proses Download Script Slowdns.. \e[0m"
-wget https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/udp-custom/slowdns/slowdns.sh && chmod +x slowdns.sh && ./slowdns.sh
-sleep 1
 echo "\e[1;32m Proses Download Script OpenVPN.. \e[0m"
 wget https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/udp-custom/openvpn/openvpn.sh && chmod +x openvpn.sh && ./openvpn.sh
 sleep 1
@@ -110,7 +107,6 @@ sleep 0,5
 clear
 cd
 rm -rf udp.sh
-rm -rf slowdns.sh
 echo -e "\e[1;32m auto reboot in 5s \e[0m"
 sleep 5
 reboot
