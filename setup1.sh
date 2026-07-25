06ef29ad586bf5731dec61329be1dcb4f72bd46d
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-25 23:58:13 +0800

    Enhance domain configuration options in setup1.sh
    Added options for domain configuration selection and downloading a script.

--- setup1.sh
+++ setup1.sh
@@ -41,9 +41,14 @@ if ! python --version 2>&1 | grep -q "Python 2.7"; then
 fi
 
 # Domain configuration
+echo "1. Use Our NT Domain Random"
 echo "2. Choose Your Own Domain"
-read -rp "Input 2: " dns
+read -rp "Input 1 or 2: " dns
 if [ "$dns" -eq 1 ]; then
+    # Download cf script and convert line endings
+    wget https://raw.githubusercontent.com/noerubio143/aa/refs/heads/main/ssh/cf
+    dos2unix cf
+    bash cf
 elif [ "$dns" -eq 2 ]; then
     read -rp "Enter Your Domain: " dom
     echo "$dom" > /var/lib/ipvps.conf
