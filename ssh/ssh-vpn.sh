f9736edc1bb935bd7d350082a8adfde7e6c1f380
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-26 14:23:55 +0800

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -325,6 +325,7 @@ wget -O m-vmess "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/ma
 wget -O m-vless "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-vless.sh"
 wget -O running "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/running.sh"
 wget -O clearcache " https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/clearcache.sh"
+wget -O m-trojan "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-trojan.sh"
 
 # menu ssh ovpn
 wget -O m-sshovpn "https://raw.githubusercontent.com/noelrubio143/aa/refs/heads/main/menu/m-sshovpn.sh"
@@ -359,6 +360,7 @@ chmod +x m-vmess
 chmod +x m-vless
 chmod +x running
 chmod +x clearcache
+chmod +x m-trojan
 
 chmod +x m-sshovpn
 chmod +x usernew
