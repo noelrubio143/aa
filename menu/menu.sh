74eb2555b54f0f935dacdde1b33eecc305a7146b
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-26 10:02:00 +0800

    Reorder menu options and fix Telegram link

--- menu/menu.sh
+++ menu/menu.sh
@@ -61,29 +61,29 @@ show_menu() {
     echo -e "\e[1;33m -------------------------------------------------\e[0m"
     echo -e ""
     echo -e "\e[1;36m 1 \e[0m: Menu SSH"
-    echo -e "\e[1;36m 3 \e[0m: Menu Vless"
-    echo -e "\e[1;36m 5 \e[0m: Menu Setting"
-    echo -e "\e[1;36m 6 \e[0m: Status Service"
-    echo -e "\e[1;36m 7 \e[0m: Clear RAM Cache"
-    echo -e "\e[1;36m 8 \e[0m: Reboot VPS"
+    echo -e "\e[1;36m 2 \e[0m: Menu Vless"
+    echo -e "\e[1;36m 3 \e[0m: Menu Setting"
+    echo -e "\e[1;36m 4 \e[0m: Status Service"
+    echo -e "\e[1;36m 5 \e[0m: Clear RAM Cache"
+    echo -e "\e[1;36m 6 \e[0m: Reboot VPS"
     echo -e "\e[1;36m x \e[0m: Exit Script"
     echo -e ""
     echo -e "\e[1;33m -------------------------------------------------\e[0m"
     echo -e "\e[1;32m Client Name \e[0m: $Name"
     echo -e "\e[1;32m Expired     \e[0m: $Exp2"
     echo -e "\e[1;32m SCRIPT BY   \e[0m: AMBERVPN"
-    echo -e "\e[1;32m Telegram Group   \e[0m: https://https://t.me/AMBER1432"
+    echo -e "\e[1;32m Telegram Group   \e[0m: https://t.me/AMBER1432"
     echo -e "\e[1;33m -------------------------------------------------\e[0m"
     echo -e ""
     read -p " Select menu :  " opt
     echo ""
     case $opt in
     1) clear ; m-sshovpn ;;
-    3) clear ; m-vless ;;
-    5) clear ; m-system ;;
-    6) clear ; running ;;
-    7) clear ; clearcache ;;
-    8) clear ; /sbin/reboot ;;
+    2) clear ; m-vless ;;
+    3) clear ; m-system ;;
+    4) clear ; running ;;
+    5) clear ; clearcache ;;
+    6) clear ; /sbin/reboot ;;
     x) exit ;;
     *) echo "Invalid selection" ; sleep 1 ;;
     esac
