7eba4c2ee6d91961c3c00f8611aaaa41124822f7
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-26 20:43:36 +0800

    Clean up service status checks and update header
    Removed status checks for TLS and non-TLS V2Ray, Trojan, and Shadowsocks services. Updated system information header and name.

--- menu/running.sh
+++ menu/running.sh
@@ -116,20 +116,6 @@ else
    status_fail2ban="${RED}  Not Running ${NC}  ( Error )"
 fi
 
-# STATUS SERVICE  TLS 
-if [[ $tls_v2ray_status == "running" ]]; then 
-   status_tls_v2ray=" ${GREEN}Running${NC} ( No Error )"
-else
-   status_tls_v2ray="${RED}  Not Running${NC}   ( Error )"
-fi
-
-# STATUS SERVICE NON TLS V2RAY
-if [[ $nontls_v2ray_status == "running" ]]; then 
-   status_nontls_v2ray=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   status_nontls_v2ray="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
 # STATUS SERVICE VLESS HTTPS
 if [[ $vless_tls_v2ray_status == "running" ]]; then
   status_tls_vless=" ${GREEN}Running${NC} ( No Error )"
@@ -143,12 +129,7 @@ if [[ $vless_nontls_v2ray_status == "running" ]]; then
 else
   status_nontls_vless="${RED}  Not Running ${NC}  ( Error )${NC}"
 fi
-# STATUS SERVICE TROJAN
-if [[ $trojan_server == "running" ]]; then 
-   status_virus_trojan=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   status_virus_trojan="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
+
 # STATUS SERVICE DROPBEAR
 if [[ $dropbear_status == "running" ]]; then 
    status_beruangjatuh=" ${GREEN}Running${NC} ( No Error )${NC}"
@@ -176,13 +157,6 @@ else
    swsdrop="${RED}  Not Running ${NC}  ( Error )${NC}"
 fi
 
-# STATUS SHADOWSOCKS
-if [[ $shadowsocks == "running" ]]; then 
-   status_shadowsocks=" ${GREEN}Running ${NC}( No Error )${NC}"
-else
-   status_shadowsocks="${RED}  Not Running ${NC}  ( Error )${NC}"
-fi
-
 
 
 # TOTAL RAM
@@ -194,13 +168,13 @@ kernelku=$(uname -r)
 
 # DNS PATCH
 #tipeos2=$(uname -m)
-Name=$"JUBAIR BRO "
+Name=$"AMBER VPN "
 Exp=$"LIFETIME FREE"
 # GETTING DOMAIN NAME
 Domen="$(cat /etc/xray/domain)"
 echo -e ""
 echo -e "\e[1;33m -------------------------------------------------\e[0m"
-echo -e "\e[1;34m                SENSI SYSTEM INFORMATION               \e[0m"
+echo -e "\e[1;34m                AMBER SYSTEM INFORMATION               \e[0m"
 echo -e "\e[1;33m -------------------------------------------------\e[0m"
 echo -e "\e[1;32m Hostname  \e[0m: $HOSTNAME"
 echo -e "\e[1;32m OS Name   \e[0m: $Tipe"
@@ -221,13 +195,9 @@ echo -e "\e[1;32m Dropbear             \e[0m: $status_beruangjatuh"
 echo -e "\e[1;32m Stunnel4             \e[0m: $status_stunnel"
 echo -e "\e[1;32m Fail2Ban             \e[0m: $status_fail2ban"
 echo -e "\e[1;32m Crons                \e[0m: $status_cron"
-echo -e "\e[1;32m Vnstat               \e[0m: $status_vnstat"
-echo -e "\e[1;32m XRAYS Vmess TLS      \e[0m: $status_tls_v2ray"
-echo -e "\e[1;32m XRAYS Vmess None TLS \e[0m: $status_nontls_v2ray"
+echo -e "\e[1;32m Vnstat               \e[0m: $status_vnstat"l
 echo -e "\e[1;32m XRAYS Vless TLS      \e[0m: $status_tls_vless"
-echo -e "\e[1;32m XRAYS Vless None TLS \e[0m: $status_nontls_vless"
-echo -e "\e[1;32m XRAYS Trojan         \e[0m: $status_virus_trojan"
-echo -e "\e[1;32m Shadowsocks          \e[0m: $status_shadowsocks"
+echo -e "\e[1;32m XRAYS Vless None TLS \e[0m: $status_nontls_vless
 echo -e "\e[1;32m Websocket TLS        \e[0m: $swstls"
 echo -e "\e[1;32m Websocket None TLS   \e[0m: $swstls"
 echo -e "\e[1;33m -------------------------------------------------\e[0m"
