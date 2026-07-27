cc1b247c6568acbc1e4288bebee485b5bbcff0d9
noelrubio143 <87189961+noelrubio143@users.noreply.github.com>
2026-07-27 13:50:45 +0800

    Update SSH & WS menu options

--- menu/m-sshovpn.sh
+++ menu/m-sshovpn.sh
@@ -9,7 +9,8 @@ echo -e "\e[33m━━━━━━━━━━━━━━━━━━━━━�
 echo -e ""
 echo -e " [\e[36m•1\e[0m] Create SSH & WS Account "
 echo -e " [\e[36m•2\e[0m] Delete SSH & WS Account "
-echo -e " [\e[36m•3\e[0m] Change Banner SSH "
+echo -e " [\e[36m•3\e[0m] List Member SSH & WS "
+echo -e " [\e[36m•4\e[0m] Change Banner SSH "
 echo -e ""
 echo -e " [\e[31m•0\e[0m] \e[31mBACK TO NT MENU\033[0m"
 echo -e ""
@@ -21,8 +22,9 @@ read -p " Select menu :  "  opt
 echo -e ""
 case $opt in
 1) clear ; usernew ; exit ;;
-2) clear ; delete ; exit ;;
-3) clear ; nano /etc/issue.net ; exit ;;
+2) clear ; hapus ; exit ;;
+3) clear ; member ; exit ;;
+4) clear ; nano /etc/issue.net ; exit ;;
 0) clear ; menu ; exit ;;
 x) exit ;;
 *) echo " wrong option " ; sleep 1 ; m-sshovpn ;;
